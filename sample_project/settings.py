64105c60040dea059f294f56bfd931d56c279e1c
LaasyaSrithebuilder <bijjalalaasyasri17@gmail.com>
2026-06-08 15:03:57 +0530

    fix: CSRF_TRUSTED_ORIGINS empty string handling

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -13,6 +13,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = [origin for origin in os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1').split(',') if origin]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
